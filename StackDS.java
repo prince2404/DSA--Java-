a382586c3bc29753722e53d0ac35b1d8fed92cb1
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-07 12:58:50 +0530

    implement stack using LL

--- StackDS.java
+++ StackDS.java
@@ -1,33 +1,75 @@
 import java.util.*;
 
 public class StackDS{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static class Stack{
-        ArrayList<Integer> list = new ArrayList<>();
+
+        public static Node head;
 
         public boolean isEmpty(){
-            return list.size() == 0;
+            return head == null;
         }
 
         public void push(int data){
-            list.add(data);
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
         }
 
         public int pop(){
             if(isEmpty()){
                 return -1;
             }
-            int value = list.get(list.size()-1);
-            list.remove(list.size()-1);
-            return value;
+            int val = head.data;
+            head = head.next;
+            return val;
+
         }
 
         public int peek(){
             if(isEmpty()){
                 return -1;
             }
-            int value = list.get(list.size()-1);
-            return value;
+            return head.data;
         }
+        // ArrayList<Integer> list = new ArrayList<>();
+
+        // public boolean isEmpty(){
+        //     return list.size() == 0;
+        // }
+
+        // public void push(int data){
+        //     list.add(data);
+        // }
+
+        // public int pop(){
+        //     if(isEmpty()){
+        //         return -1;
+        //     }
+        //     int value = list.get(list.size()-1);
+        //     list.remove(list.size()-1);
+        //     return value;
+        // }
+
+        // public int peek(){
+        //     if(isEmpty()){
+        //         return -1;
+        //     }
+        //     int value = list.get(list.size()-1);
+        //     return value;
+        // }
     }
 
     public static void main(String args[]){
