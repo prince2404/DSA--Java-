aab8d700114bf979d9d247ff176c434379035599
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-10 11:17:08 +0530

    check valid parenthesis

--- StackInbuilt.java
+++ StackInbuilt.java
@@ -95,6 +95,33 @@ public class StackInbuilt{
         System.out.println();
     }
 
+    public static boolean checkValidParenthesis(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if((s.peek() == '(' && ch == ')') || 
+                (s.peek() == '{' && ch == '}') || 
+                (s.peek() == '[' && ch == ']')){
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        if(s.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public static void main(String args[]){
         // String str = "abc";
         // System.out.println(reverseStr(str));
@@ -122,7 +149,10 @@ public class StackInbuilt{
         // int stock[] = {100, 80, 60, 70, 60, 85, 100};
         // stockSpan(stock);
 
-        int arr[] = {6,8,0,1,3};
-        nextGreaterElement(arr);
+        // int arr[] = {6,8,0,1,3};
+        // nextGreaterElement(arr);
+
+        String str = "({})[]";
+        System.out.println(checkValidParenthesis(str));
     }
 }
