8f676e96e58199b4c2989a35a3d0c8b4aa06753a
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-26 20:46:27 +0530

    queue implementation using arrays

---
+++ QueueDS.java
@@ -0,0 +1,62 @@
+public class QueueDS{
+    public static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+
+        public Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public boolean isEmpty(){
+            return rear == -1;
+        }
+        public boolean isFull(){
+            return rear == size-1;
+        }
+
+        public void add(int data){
+            if(isFull()){
+                System.out.println("queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int val = arr[0];
+            for(int i = 0; i < rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return val;
+        }
+
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String args[]){
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.remove());
+        }
+
+
+    }
+}
