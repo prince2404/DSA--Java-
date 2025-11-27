9e33ec6ec177cc72f7450baa356276640cf379e0
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-27 21:59:09 +0530

    queue using two stack , add-> O(1)

--- QueueDS.java
+++ QueueDS.java
@@ -9,15 +9,7 @@ public class QueueDS{
         }
 
         public void add(int data){
-            while(!s1.isEmpty()){
-                s2.push(s1.pop());
-            }
-
             s1.push(data);
-
-            while(!s2.isEmpty()){
-                s1.push(s2.pop());
-            }
         }
 
         public int remove(){
@@ -25,7 +17,15 @@ public class QueueDS{
                 System.out.println("Queue is empty");
                 return -1;
             }
-            return s1.pop();
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            int top = s2.pop();
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            return top;
         }
 
         public int peek(){
@@ -33,7 +33,15 @@ public class QueueDS{
                 System.out.println("Queue is empty");
                 return -1;
             }
-            return s1.peek();
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            int top = s2.peek();
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            return top;
         }
     }
     
