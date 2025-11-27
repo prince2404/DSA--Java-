09ec897faa93bb36fdc8be949fd5ef028c9c36ba
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-27 20:21:53 +0530

    Queue implementation using JCF

--- QueueDS.java
+++ QueueDS.java
@@ -1,52 +1,8 @@
+import java.util.*;
 public class QueueDS{
-    public static class Node{
-        int data;
-        Node next;
-
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public static class Queue{
-        public static Node head;
-        public static Node tail;
-        
-
-        public boolean isEmpty(){
-            return head == null;
-        }
-
-        public void add(int data){
-            Node newNode = new Node(data);
-            if(head == null){
-                head = tail = newNode;
-                return;
-            }
-            tail.next = newNode;
-            tail = newNode;
-        }
-
-        public int remove(){
-            if(isEmpty()){
-                System.out.println("queue is empty");
-                return -1;
-            }
-            int val = head.data;
-            head = head.next;
-            return val;
-        }
-
-        public int peek(){
-            if(isEmpty()){
-                System.out.println("queue is empty");
-                return -1;
-            }
-            return head.data;
-        }
-    }
+    
     public static void main(String args[]){
-        Queue q = new Queue();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
