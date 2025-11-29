2be7c89ed7fbd9503f7f491ac70187c986c65dcf
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-11-29 16:04:16 +0530

    interleave 2 halves of a queue

--- QueueProblems.java
+++ QueueProblems.java
@@ -22,9 +22,43 @@ public class QueueProblems {
 
     }
 
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> first = new ArrayDeque<>();
+        int size = q.size();
+        for(int i = 1; i <= size/2; i++){
+            first.add(q.remove());
+        }
+
+        while(!first.isEmpty()){
+            q.add(first.remove());
+            q.add(q.remove());
+        }
+    }
+
+
+
     public static void main(String args[]){
-        String str = "aabccxb";
-        firstNonRepeating(str);
+        // String str = "aabccxb";
+        // firstNonRepeating(str);
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        interLeave(q);
+
+        while(!q.isEmpty()){
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
     }
     
 }
