6667599cea69f792a624108450ef0b437be7b666
prince2404 <166131044+prince2404@users.noreply.github.com>
2025-12-02 15:13:00 +0530

    activity selection updated

--- GreedyAlgo.java
+++ GreedyAlgo.java
@@ -2,21 +2,32 @@ import java.util.*;
 
 public class GreedyAlgo {
     public static void main(String args[]){
-        int start[] = {1,3,0,5,8,5};
-        int end[] = {2,4,6,7,9,9};
+        int start[] = {0,1,3,5,5,8};
+        int end[] = {6,2,4,7,9,9};
+        int n = start.length;
+
+        int act[][] = new int[n][3];
+        for(int i = 0; i < n; i++){
+            act[i][0] = i;
+            act[i][1] = start[i];
+            act[i][2] = end[i];
+        }
+
+        Arrays.sort(act, Comparator.comparingInt(o -> o[2]));
+
 
         int count = 0;
         ArrayList<Integer> ans = new ArrayList<>();
         //1st activity
         count = 1;
-        ans.add(0);
+        ans.add(act[0][0]);
 
-        int lastEnd = end[0];
-        for(int i = 1; i < start.length; i++){
-            if(start[i] >= lastEnd){
+        int lastEnd = act[0][2];
+        for(int i = 1; i < n; i++){
+            if(act[i][1] >= lastEnd){
                 count++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(act[i][0]);
+                lastEnd = act[i][2];
             }
         }
 
