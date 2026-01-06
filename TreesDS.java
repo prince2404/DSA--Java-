3aee00d9904fdf4bf1fe580990d20130a933c402
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-01-06 14:14:11 +0530

    Preorder traversal

--- TreesDS.java
+++ TreesDS.java
@@ -24,12 +24,22 @@ public class TreesDS {
 
             return newNode;
         }
+
+        public static void preorder(Node root){
+            if(root == null){
+                // System.out.print(-1 + " ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
     public static void main(String args[]){
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTrees tree = new BinaryTrees();
         Node root = tree.buildTrees(nodes);
-        System.out.println(root.data);
+        tree.preorder(root);
 
     }
     
