925b3475a9d2a78f0ba61fe4a2f2b874c4fee654
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-01-13 11:45:52 +0530

    Bottom view of a tree

--- TopViewOfTree.java
+++ TopViewOfTree.java
@@ -25,6 +25,9 @@ public class TopViewOfTree {
     }
 
     public static void topView(Node root){
+        if(root == null){
+            return;
+        }
         Queue<Info> q = new LinkedList<>();
         HashMap<Integer, Node> map = new HashMap<>();
 
@@ -64,6 +67,48 @@ public class TopViewOfTree {
         }
         System.out.println();
     }
+
+    public static void bottomView(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0;
+        int max = 0;
+
+        q.add(new Info(root, 0));
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Info curr = q.remove();
+            if(curr == null){
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                map.put(curr.hd, curr.node);
+
+                if(curr.node.left != null){
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min = Math.min(min, curr.hd-1);
+                }
+
+                if(curr.node.right != null){
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max = Math.max(max, curr.hd+1);
+                }   
+            }
+        }
+
+        for(int i = min; i <= max; i++){
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -73,7 +118,16 @@ public class TopViewOfTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        topView(root);
+        // Node root = new Node(1);
+        // root.left = new Node(2);
+        // root.right = new Node(3);
+        // root.left.right = new Node(4);
+        // root.left.right.right = new Node(5);
+        // root.left.right.right.right = new Node(6);
+
+        // topView(root);
+        bottomView(root);
+    
 
     }
     
