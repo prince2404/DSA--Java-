a72b62ec1846f7b76ce2b0789701435626d030af
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-01-17 10:16:15 +0530

    Lowest Common Ancestor

--- HeightOfTree.java
+++ HeightOfTree.java
@@ -119,6 +119,45 @@ public class HeightOfTree {
         kthLevel(root.right, level+1, k);
 
     }
+
+    public static Node lowestCommonAncestor(Node root, int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i = 0;
+
+        for(; i < path1.size() && i < path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        return path1.get(i-1);
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if(root == null){
+            return false;
+        }
+
+        path.add(root);
+
+        if(root.data == n){
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight){
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
+    }
    
     public static void main(String args[]){
         Node root = new Node(1);
@@ -129,8 +168,14 @@ public class HeightOfTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
+        int n1 = 4;
+        int n2 = 5;
+
+        Node lca = lowestCommonAncestor(root, n1, n2);
+        System.out.println(lca.data);
+
         
-        kthLevel(root, 1, 2);
+        // kthLevel(root, 1, 2);
 
         // Node subRoot = new Node(2);
         // subRoot.left = new Node(4);
