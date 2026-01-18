9fb9cf8b2e91ea0c3c84f39684bca7cd8e727bde
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-01-18 10:01:33 +0530

    Min distance between two nodes

--- HeightOfTree.java
+++ HeightOfTree.java
@@ -120,43 +120,88 @@ public class HeightOfTree {
 
     }
 
-    public static Node lowestCommonAncestor(Node root, int n1, int n2){
-        ArrayList<Node> path1 = new ArrayList<>();
-        ArrayList<Node> path2 = new ArrayList<>();
+    // public static Node lowestCommonAncestor(Node root, int n1, int n2){
+    //     ArrayList<Node> path1 = new ArrayList<>();
+    //     ArrayList<Node> path2 = new ArrayList<>();
 
-        getPath(root, n1, path1);
-        getPath(root, n2, path2);
+    //     getPath(root, n1, path1);
+    //     getPath(root, n2, path2);
 
-        int i = 0;
+    //     int i = 0;
 
-        for(; i < path1.size() && i < path2.size(); i++){
-            if(path1.get(i) != path2.get(i)){
-                break;
-            }
-        }
-        return path1.get(i-1);
+    //     while(i < path1.size() && i < path2.size()){
+    //         if(path1.get(i) != path2.get(i)){
+    //             break;
+    //         }
+    //         i++;
+    //     }
+    //     return path1.get(i-1);
+    // }
+
+    // public static boolean getPath(Node root, int n, ArrayList<Node> path){
+    //     if(root == null){
+    //         return false;
+    //     }
+
+    //     path.add(root);
+
+    //     if(root.data == n){
+    //         return true;
+    //     }
+
+    //     boolean foundLeft = getPath(root.left, n, path);
+    //     boolean foundRight = getPath(root.right, n, path);
+
+    //     if(foundLeft || foundRight){
+    //         return true;
+    //     }
+
+    //     path.remove(path.size()-1);
+    //     return false;
+    // }
+
+    public static int minDist(Node root, int n1, int n2){
+        Node lca = lowestCommonAncestor(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
     }
 
-    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+    public static Node lowestCommonAncestor(Node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftLCA = lowestCommonAncestor(root.left, n1, n2);
+        Node rightLCA = lowestCommonAncestor(root.right, n1, n2);
+
+        if(rightLCA == null){
+            return leftLCA;
+        }
+        if(leftLCA == null){
+            return rightLCA;
+        }
+
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n){
         if(root == null){
-            return false;
+            return -1;
         }
-
-        path.add(root);
-
         if(root.data == n){
-            return true;
+            return 0;
         }
+        int leftlcaDist = lcaDist(root.left, n);
+        int rightlcaDist = lcaDist(root.right, n);
 
-        boolean foundLeft = getPath(root.left, n, path);
-        boolean foundRight = getPath(root.right, n, path);
-
-        if(foundLeft || foundRight){
-            return true;
+        if(leftlcaDist == -1 && rightlcaDist == -1){
+            return -1;
+        }else if(leftlcaDist == -1){
+            return rightlcaDist + 1;
+        }else{
+            return leftlcaDist + 1;
         }
-
-        path.remove(path.size()-1);
-        return false;
     }
    
     public static void main(String args[]){
@@ -169,10 +214,12 @@ public class HeightOfTree {
         root.right.right = new Node(7);
 
         int n1 = 4;
-        int n2 = 5;
+        int n2 = 6;
 
-        Node lca = lowestCommonAncestor(root, n1, n2);
-        System.out.println(lca.data);
+        System.out.println(minDist(root, n1, n2));
+
+        // Node lca = lowestCommonAncestor(root, n1, n2);
+        // System.out.println(lca.data);
 
         
         // kthLevel(root, 1, 2);
