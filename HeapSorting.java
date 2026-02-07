dad7af920bf707c10c501ae96f65f43bcf4d8d23
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-02-07 08:28:53 +0530

    Heap sort

---
+++ HeapSorting.java
@@ -0,0 +1,47 @@
+public class HeapSorting {
+    public static void heapSort(int arr[]){
+        int n = arr.length;
+
+        for(int i = n/2-1; i >= 0; i--){
+            heapify(arr, n, i);
+        }
+
+        for(int i = n-1; i > 0; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int arr[], int n, int i){
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int max = i;
+
+        if(left < n && arr[left] > arr[max]){
+            max = left;
+        }
+        if(right < n && arr[right] > arr[max]){
+            max = right;
+        }
+
+        if(max != i){
+            int temp = arr[i];
+            arr[i] = arr[max];
+            arr[max] = temp;
+
+            heapify(arr, n, max);
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {1,2,4,5,3};
+        heapSort(arr);
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+    
+}
