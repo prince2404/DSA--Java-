46bfd15d7801f9f8ea7fa12d55e8f16edf44e34c
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-02-13 12:12:11 +0530

    Majority elements problem

---
+++ HashMapQue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class HashMapQue {
+    public static void majorityElements(int nums[]){
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+
+        for(Map.Entry<Integer,Integer> e : map.entrySet()){
+            if(e.getValue() > n/3){
+                System.out.println(e.getKey());
+            }
+        }
+    }
+    public static void main(String args[]){
+        int nums[] = {1,3,2,5,1,3,1,5,1};
+        majorityElements(nums);
+    }
+    
+}
