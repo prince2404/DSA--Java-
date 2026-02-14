e5ff1fe19d98326e8596b059b13927b2cab12ccd
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-02-14 18:15:29 +0530

    union and intersection of two arrays

--- HashMapQue.java
+++ HashMapQue.java
@@ -21,11 +21,44 @@ public class HashMapQue {
         }
         return set.size();
     }
+
+    public static int union(int arr1[], int arr2[]){
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : arr1){
+            set.add(n);
+        }
+        for(int n : arr2){
+            set.add(n);
+        }
+        return set.size();
+    }
+
+    public static int intersection(int arr1[], int arr2[]){
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+
+        for(int n : arr1){
+            set.add(n);
+        }
+        for(int n : arr2){
+            if(set.contains(n)){
+                result.add(n);
+            }
+        }
+
+        return result.size();
+    }
+
     public static void main(String args[]){
         // int nums[] = {1,3,2,5,1,3,1,5,1};
         // majorityElements(nums);
-        int nums[] = {4,3,2,5,6,7,3,4,2,1};
-        System.out.println(countDistinctElements(nums));
+        // int nums[] = {4,3,2,5,6,7,3,4,2,1};
+        // System.out.println(countDistinctElements(nums));
+
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,2,9,4};
+        System.out.println(union(arr1,arr2));
+        System.out.println(intersection(arr1,arr2));
     }
     
 }
